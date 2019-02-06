3599770ee52408cd74437908959cd9326afe4258
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-02-06 10:50:12 -0700

    append card, delete card button, timer, refactor

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ var maxNumber = document.querySelector('#max-number');
 
 var challengerName1 = document.querySelector('#challenger-name1');
 var challengerName2 = document.querySelector('#challenger-name2');
-var challenger1Name = document.querySelectorAll('.challenger1-name');
-var challenger2Name = document.querySelectorAll('.challenger2-name');
+
+var challenger1Name = document.querySelector('.challenger1-name');
+var challenger2Name = document.querySelector('.challenger2-name');
+
 var feedbackMessage1 = document.querySelector('#ch1-feedback-message');
 var feedbackMessage2 = document.querySelector('#ch2-feedback-message');
 var updateButton = document.querySelector('#update-button');
@@ -20,22 +22,56 @@ var clearGameButton = document.querySelector('#clear-button');
 var resetButton = document.querySelector('#reset-button');
 var generatedRandomNumber = randomNumber(1,100);
 
+var card = document.querySelector('.right-column')
 var winnerName = document.querySelector('#winner-name');
 
 guessSubmitButton.addEventListener('click', playGame);
 clearGameButton.addEventListener('click', clearGame);
-updateButton.addEventListener('click', updateRange);
+updateButton.addEventListener('click', update);
 resetButton.addEventListener('click', newGame);
 
 // Function that calls Feedback functions when submit button is hit 
 function playGame () {
   emptyNameInputs(); 
   emptyGuessInputs();
-  challengersGuesses();
-  feedback1();
-  feedback2();
-  challengerNames();
-  updateWinnerName();
+  startTimer ();
+  challengersGuesses(challenger1guess, guessPlaceholder1);
+  challengersGuesses(challenger2guess, guessPlaceholder2);
+  guessCounter();
+  feedback(challenger1guess, feedbackMessage1);
+  feedback(challenger2guess, feedbackMessage2);
+  challengerNames(challenger1Name, challengerName1);
+  challengerNames(challenger2Name, challengerName2);
+}
+
+function update () {
+  customRange(minRangeSet, maxRangeSet);
+}
+
+function clearGame(){
+  clearInputs(challenger1guess, challenger2guess);
+  clearInputs(challengerName1, challengerName2);
+  clearInputs(minRangeSet, maxRangeSet);
+  nameError('challenger1','hidden');
+  nameError('challenger2','hidden');
+  guessError('challenger1','hidden');
+  guessError('challenger2','hidden');
+  minMaxError('min','hidden');
+  minMaxError('max','hidden');
+  defaultMinMaxRange();
+}
+
+// Sets game to initial state
+function newGame(){
+  event.preventDefault();
+  clearGame();
+};
+
+// Makes sure min and max are numerical values then creates a random number 
+function randomNumber(min, max) {
+  min = parseInt(min);
+  max = parseInt(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Testing Random number generater in the console
@@ -47,108 +83,72 @@ function minMaxError(minOrmax, visibleOrhidden) {
 }
 
 // Update the current range that the user enters and randomly generates a number between that range
-function updateRange() {
 
-  var minRange = parseInt(minRangeSet.value);
-  var maxRange = parseInt(maxRangeSet.value);
-  if (!minRangeSet.value && !maxRangeSet.value) {
+function customRange(customMinRange, customMaxRange) {
+
+  customMinRange = parseInt(customMinRange.value);
+  customMaxRange = parseInt(customMaxRange.value);
+
+  if (!customMinRange && !customMaxRange) {
     minMaxError('min','visible');
     minMaxError('max','visible');
     return;
-  } else if (!minRangeSet.value) {
+  } else if (!customMinRange) {
     minMaxError('min','visible');
     minMaxError('max','hidden');
-  } else if (!maxRangeSet.value) {
+  } else if (!customMaxRange) {
     minMaxError('max','visible');
     minMaxError('min','hidden');
-  } else if (minRange >= maxRange) {
+  } else if (customMinRange >= customMaxRange) {
     alert('Please make max range number larger then min range number');
   } else {
     minMaxError('min','hidden');
     minMaxError('max','hidden');
-    minNumber.innerText = minRange;
-    maxNumber.innerText = maxRange;
-    generatedRandomNumber = randomNumber(minRange,maxRange);
+    minNumber.innerText = customMinRange;
+    maxNumber.innerText = customMaxRange;
+    generatedRandomNumber = randomNumber(customMinRange,customMaxRange);
   }
   console.log(generatedRandomNumber);
 };
 
 
-function challengersGuesses() {
+//Checks that challenger guesses are in the range and if they are then assigns them.
+function challengersGuesses(challengerGuess, guessPlaceholder) {
 
+  challengerGuess = parseInt(challengerGuess.value);
   minNumber = parseInt(minNumber.innerText);
   maxNumber = parseInt(maxNumber.innerText);
 
-  if (!challenger1guess.value) {
-    guessPlaceholder1.innerText = '97'
-  } else if (challenger1guess.value < minNumber || challenger1guess.value > maxNumber) {
-    alert('Nope that is outside of the range')
+  if (!challengerGuess) {
+    guessPlaceholder.innerText = '--'
+  } else if (challengerGuess < minNumber || challengerGuess > maxNumber) {
+    alert('Nope, that is outside of the range');
   } else {
-    guessPlaceholder1.innerText = challenger1guess.value;
-  }
-
-  if (!challenger2guess.value) {
-    guessPlaceholder2.innerText = '3'
-    } else if (challenger2guess.value < minNumber || challenger2guess.value > maxNumber) {
-      alert('Nope that is outside of the range')
-    } else {
-    guessPlaceholder2.innerText = challenger2guess.value;
+    guessPlaceholder.innerText = challengerGuess;
   }
 }
 
-// Makes sure min and max are numerical values then creates a 
-function randomNumber(min, max) {
-  min = parseInt(min);
-  max = parseInt(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Feedback message for challenger 1
-function feedback1() {
-  var givenNumber = parseInt(challenger1guess.value);
-  if (givenNumber < generatedRandomNumber) {
-    feedbackMessage1.innerText = 'that\'s too low'
-  } else if (givenNumber > generatedRandomNumber) {
-    feedbackMessage1.innerText = 'that\'s too high'
-  } else if (givenNumber === generatedRandomNumber) {
-    feedbackMessage1.innerText = 'BOOM!'
+function feedback(challengerGuess, feedbackMessage) {
+  challengerGuess = parseInt(challengerGuess.value);
+  if (!challengerGuess){
+    feedbackMessage.innerText = 'Stop messing around'
+  } else if (challengerGuess < generatedRandomNumber) {
+    feedbackMessage.innerText = 'that\'s too low'   
+  } else if (challengerGuess > generatedRandomNumber) {
+    feedbackMessage.innerText = 'that\'s too high'
   } else {
-    feedbackMessage1.innerText = 'that\'s too high'
-  }
+    feedbackMessage.innerText = 'BOOM!'
+    updateWinnerName();
+  } 
 }
 
-// Feedback message for challenger 2
-function feedback2() {
-  var givenNumber = parseInt(challenger2guess.value);
-  if (givenNumber < generatedRandomNumber) {
-    feedbackMessage2.innerText = 'that\'s too low'
-  } else if (givenNumber > generatedRandomNumber) {
-    feedbackMessage2.innerText = 'that\'s too high'
-  } else if (givenNumber === generatedRandomNumber) {
-    feedbackMessage2.innerText = 'BOOM!'
+function challengerNames(challengerName, challengerNameInput) {
+  if (!challengerNameInput.value) {
+    challengerName.innerText = 'Challenger Name';
   } else {
-    feedbackMessage2.innerText = 'that\'s too low'
+    challengerName.innerText = challengerNameInput.value;
   }
-}
-
-// Changes name placeholders to user inputs 
-function challengerNames() {
-  for ( var i = 0; i < challenger1Name.length; i++) {
-    if (!challengerName1.value) {
-      challenger1Name[i].innerText = 'Challenger 1 Name'
-    } else {
-      challenger1Name[i].innerText = challengerName1.value;
-    }
 };
-
-  for ( var i = 0; i < challenger2Name.length; i++) { 
-    if(!challengerName2.value) {
-      challenger2Name[i].innerText = 'Challenger 2 Name'
-    } else {
-      challenger2Name[i].innerText = challengerName2.value;
-    }
-};
-}
 
 function guessError(challenger1Orchallenger2, visibleOrhidden) {
   const error = document.querySelector(`#${challenger1Orchallenger2}-guess-error`);
@@ -192,58 +192,89 @@ function emptyNameInputs() {
   }
 }
 
-function clearGame(){
-  challenger1guess.value = '';
-  challenger2guess.value = '';
-  challengerName1.value = '';
-  challengerName2.value = '';
-  minRangeSet.value = '';
-  maxRangeSet.value = '';
-  nameError('challenger1','hidden');
-  nameError('challenger2','hidden');
-  guessError('challenger1','hidden');
-  guessError('challenger2','hidden');
-  minMaxError('min','hidden');
-  minMaxError('max','hidden');
+function clearInputs(field1, field2) {
+  field1.value = '';
+  field2.value = '';
 }
 
-// Sets game to initial state
-function newGame(){
-  clearGame();
-  challengerNames();
-  challengersGuesses();
-  feedback1();
-  feedback2();
-  var generatedRandomNumber = randomNumber(1,100);
-  console.log(generatedRandomNumber);
+function defaultMinMaxRange() {
   minNumber.innerText = '1';
   maxNumber.innerText = '100';
-};
-
-//Sets the input field to only take numbers
-// function numericalValueOnly(minRangeSet, maxRangeSet challenger1guess, challenger2guess){
-//   var numerical = 0-9;
-//   if ( minRangeSet())
-//     {
+  generatedRandomNumber = randomNumber(1,100);
+  console.log(generatedRandomNumber);
+}
 
 //Sets the winner name in the right column if they guess the exact number
 function updateWinnerName(){
-  // console.log ('guesses',typeof(challenger1guess.value),parseInt(challenger1guess.value),parseInt(challenger2guess.value),generatedRandomNumber);
   if (parseInt(challenger1guess.value) === generatedRandomNumber){
-   return winnerName.innerText = challengerName1.value;
+   displayCard(challengerName1);
+   totalGuesses = 0;
   }else if (parseInt(challenger2guess.value) === generatedRandomNumber) {
-    return winnerName.innerText = challengerName2.value;
+    displayCard(challengerName2);
+    totalGuesses = 0;
   }else {
-    return winnerName.innerText = 'Challenger Name';
+    return;
   }
 }
 
+function deleteCard(event) {
+  event.target.closest('.card-results').remove();
+}
 
+// Guess Counter 
+var totalGuesses = 0;
+function guessCounter () {
+  totalGuesses += 2;
+}
 
+// Start Timer
+var startTime = 0;
+function startTimer () {
+  startTime = Date.now();
+}
 
+// End Timer
+var endTime = 0;
+function endTimer () {
+  var endTime = Date.now();
+  var totalTime = (endTime - startTime) / 1000;
+  console.log('total ' + totalTime);
+  minutes = totalTime.toFixed(2);
+}
 
-
-
-
-
-
+function displayCard(winner) {
+  let winningCard = `
+  <section class="card-results">
+    <article class="card-header">
+      <p class="challenger1-name">${challengerName1.value}</p>
+      <p> vs </p>
+      <p class="challenger2-name">${challengerName2.value}</p>
+    </article>
+    <hr>
+    <article class="card-body">
+      <p id="winner-name">${winner.value}</p>
+      <p class="winner">WINNER</p>
+    </article>
+    <hr>
+    <article class="card-footer">
+      <div class="footer-flex">
+        <p class="bold">${totalGuesses}</p>
+        <p class="guesses">guesses</p>
+      </div>
+      <div class="footer-flex">
+        <p class="bold">1.35 </p>
+        <p>minutes</p>
+      </div>
+      <div>
+        <button class="delete-button">X</button>
+      </div>
+    </article>  
+  </section>`
+  endTimer ();
+  card.innerHTML += winningCard;
+  card.addEventListener('click', function(event) {
+  if(event.target.classList.contains('delete-button')) {
+    deleteCard(event);
+  }
+});
+}
